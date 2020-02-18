b529e3f6c96139b13d12cf4ef149b6bc88ef03b2
Marci(Admin) <marton.szatzker@gmail.com>
2020-02-18 23:34:06 +0100

    First Commit

---
+++ src/test/java/com/tzkinformatics/FirstBoot/FirstBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzkinformatics.FirstBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
